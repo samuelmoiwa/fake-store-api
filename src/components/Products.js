cc44c295718e111a9137a5e341fcc2843a6cb920
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-12-27 23:09:27 +0000

    add Products.js

---
+++ src/components/Products.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Products() {
+
+  return (
+  <div>
+    <div className='product_card'>
+      <div className='product_image'> image </div>
+      <h1 className='product_title'> title </h1>
+      <h1 className='product_category'> category </h1>
+      <h1 className='product_price'> price </h1>
+    </div>
+  </div>
+  );
+  
+}
+
+export default Products;
